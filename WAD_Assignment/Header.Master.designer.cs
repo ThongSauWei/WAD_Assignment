c69e2fd045929f7db80c5a369ce74e0a52a53318
sauweiiiii~ <114349729+ThongSauWei@users.noreply.github.com>
2023-12-09 02:13:04 +0800

    Header gua

--- WAD_Assignment/Header.Master.designer.cs
+++ WAD_Assignment/Header.Master.designer.cs
@@ -11,25 +11,16 @@ namespace WAD_Assignment
 {
 
 
-    public partial class Testing
+    public partial class Header
     {
 
         /// <summary>
-        /// Button1 control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
-        /// LoginView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LoginView LoginView1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
