6881a28e5d21b1e0ca539d65b97da3c338235758
sauweiiiii~ <114349729+ThongSauWei@users.noreply.github.com>
2023-12-11 15:02:46 +0800

    header

---
+++ WAD_Assignment/Detail/abc/Testing.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Header.Master" AutoEventWireup="true" CodeBehind="Testing.aspx.cs" Inherits="WAD_Assignment.Detail.abc.Testing" %>
+
+<asp:Content ID="ContentPlaceHolder" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div>
+        </div>
+</asp:Content>
+
--- WAD_Assignment/Header.Master.designer.cs
+++ WAD_Assignment/Header.Master.designer.cs
@@ -24,6 +24,15 @@ namespace WAD_Assignment
         protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
 
         /// <summary>
+        /// cinemaLogo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlImage cinemaLogo;
+
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
